5225c28097f4dcb7cf611fbea76d5a81b27fd0a4
Aidan McReynolds <akmcr@yahoo.ca>
2019-09-26 14:08:32 -0700

    hello

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
+cd /tmp
 wget https://dl.google.com/go/go1.13.1.linux-amd64.tar.gz
-tar -C /usr -xzf go1.13.1.linux-amd64.tar.gz
+tar -C / -xzf go1.13.1.linux-amd64.tar.gz
 export PATH=$PATH:/usr/
 bin/goer
